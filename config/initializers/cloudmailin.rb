84d4776145f370bd953a529825e28f0c7a493ef4
David Benjoya dave@benjoya.com <dave@benjoya.com>
2021-01-06 17:55:04 +0100

    fixed cloudnailin config

--- config/initializers/cloudmailin.rb
+++ config/initializers/cloudmailin.rb
@@ -1,14 +1,9 @@
 ActionMailer::Base.add_delivery_method :cloudmailin, Mail::SMTP,
   address: ENV['CLOUDMAILIN_HOST'],
   port: 587,
-  domain: 'yourdomain.com',
+  domain: 'scroob.com',
   user_name: ENV['CLOUDMAILIN_USERNAME'],
   password: ENV['CLOUDMAILIN_PASSWORD'],
   authentication: 'plain',
   enable_starttls_auto: true
 
-# config/environments/production.yml
-config.action_mailer.delivery_method = :cloudmailin
-
-# Default url for mail
- config.action_mailer.default_url_options = { host: 'cloudmailin.com' }
