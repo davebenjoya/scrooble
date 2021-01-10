5035c9e3f3b984a9072036e5c0162f023db3e501
David Benjoya dave@benjoya.com <dave@benjoya.com>
2021-01-10 18:22:17 +0100

    changed color of current player in index

--- app/javascript/components/game_index.js
+++ app/javascript/components/game_index.js
@@ -5,17 +5,29 @@ const gameIndex = ()  => {
     document.querySelectorAll(".stored-game").forEach(game => {
     let playerList = ``
       const players = game.dataset.players.replace("[", "").replace("]", "").replaceAll(/\},\s*\{/g, "@@@");
-      const arr = players.split("@@@")
-      arr.forEach(player => {
-        const name =  player.replace(/[\{\}]/, "")
+      const arr = players.split("@@@");
+      let current = 0;
+      arr.forEach((player, index) => {
+        let name =  player.replace(/[\{\}]/, "")
         .split(",")[0].replace(/name/, "")
         .replaceAll(":", "").replaceAll(/\'/g, "")
         .replaceAll(/\"/g, "").replaceAll("=>", "").trim();
-        playerList += (name + ", ")
-      })
-      const lst = playerList.substr(0, playerList.length - 2)
-      game.querySelector(".players").insertAdjacentText('beforeend', lst);
-      console.log(lst);
+        // console.log('current user ' + current_user);
+        if (name === document.querySelector(".your-games").dataset.username) {
+          name = "You, "
+        } else {
+          name += ", "
+        }
+        if (parseInt(game.querySelector(".players").dataset.current) === index) {
+          name = `<span class= "current-player">${name}, </span>`
+        }
+
+        playerList += name;
+
+      });
+      // game.querySelectorAll(current)
+      const lst = playerList.substr(0, playerList.length - 2).replaceAll(", ,", ",");
+      game.querySelector(".players").insertAdjacentHTML('beforeend', lst);
     })
   }
 }
