61cb9a77a078f16964db563a856b4548f08edfb5
David Benjoya dave@benjoya.com <dave@benjoya.com>
2021-01-11 13:56:51 +0100

    staggered player lists on index page

--- app/javascript/components/game_index.js
+++ app/javascript/components/game_index.js
@@ -3,11 +3,18 @@ const gameIndex = ()  => {
 
   if (yourGames) {
     document.querySelectorAll(".stored-game").forEach(game => {
-    let playerList = ``
       const players = game.dataset.players.replace("[", "").replace("]", "").replaceAll(/\},\s*\{/g, "@@@");
-      const arr = players.split("@@@");
-      let current = 0;
-      arr.forEach((player, index) => {
+      const playersArr = players.split("@@@");
+      let current = parseInt(game.querySelector(".players").dataset.current);
+      const staggeredArrayBegin = playersArr.slice(current);
+      const staggeredArrayEnd = playersArr.slice(0, current);
+      const staggeredArray = staggeredArrayBegin.concat(staggeredArrayEnd);
+
+      console.log('staggeredArray ' + staggeredArray.length);
+      let playerList = ``;
+
+        let alertPlayer = "";
+      staggeredArray.forEach((player, index) => {
         let name =  player.replace(/[\{\}]/, "")
         .split(",")[0].replace(/name/, "")
         .replaceAll(":", "").replaceAll(/\'/g, "")
@@ -15,19 +22,44 @@ const gameIndex = ()  => {
         // console.log('current user ' + current_user);
         if (name === document.querySelector(".your-games").dataset.username) {
           name = "You, "
+          switch (index) {
+            case 0:
+              alertPlayer = " — It's your turn.";
+              break;
+            case 1:
+              alertPlayer = " — You're up next.";
+              break;
+              default: null;
+          }
         } else {
           name += ", "
         }
-        if (parseInt(game.querySelector(".players").dataset.current) === index) {
-          name = `<span class= "current-player">${name}, </span>`
-        }
+        // console.log('playersArr.length  ' + playersArr);
+          // if (parseInt(game.querySelector(".players").dataset.current) === index) {
+          //   if (index === playersArr.length - 1 ) {
+          //     name = `<span class= "current-player">${name}  </span>`
+          //   } else {
+          //     name = `<span class= "current-player">${name}, </span>`
+          //   }
+          // }
+          //   if (parseInt(game.querySelector(".players").dataset.current) === index) {
+              // console.log('indefgt hfhx  ' + index);
+          //     name = `<span class= "current-player">${name}, </span>`
+          //   }
+          //   } else {
+          //   if (parseInt(game.querySelector(".players").dataset.current) === index) {
+          //     name = `<span class= "current-player">${name}  </span>`
+          //   }
+          // }
 
         playerList += name;
 
       });
       // game.querySelectorAll(current)
-      const lst = playerList.substr(0, playerList.length - 2).replaceAll(", ,", ",");
+      // console.log('alertPlayer' + alertPlayer);
+      const lst = playerList.replace(/,\s*$/, "").replaceAll(", ,", ",");
       game.querySelector(".players").insertAdjacentHTML('beforeend', lst);
+      game.querySelector(".players").insertAdjacentHTML('beforeend', alertPlayer);
     })
   }
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,71 +20,71 @@ User.create!([{ email: 'deike@benjoya.com', password: 'blue78!', username: 'Deik
 
 
 
-Game.create({ name: 'Friday 3AM', completed: false,
-              current_player: 0,
-              players:  [
-                         { 'name': 'Dave', 'score': '18', 'current_letters': 'RXDFOEB' },
-                         { 'name': 'Deike', 'score': '22', 'current_letters': 'LTAUWVQ' },
-                         { 'name': 'Luna', 'score': '35', 'current_letters': 'ZHGNIOC' },
-                         { 'name': 'Ed', 'score': '34', 'current_letters': 'USYKTER' }
-                         ],
-              letter_grid: "_ _ _ _ _ _ B R A I N _ _ _ _
-                            _ _ _ _ _ _ O _ _ D _ _ _ _ _
-                            _ _ _ _ C R A S H _ _ _ _ _ _
-                            _ _ _ _ O _ _ U _ _ _ _ _ _ _
-                            _ _ _ _ O _ _ R _ _ _ _ _ _ _
-                            _ _ G A L L O P _ _ _ _ _ _ _
-                            _ _ _ _ _ _ _ R _ _ _ _ _ _ _
-                            _ _ _ _ _ _ _ I _ _ _ _ _ _ _
-                            _ _ _ _ _ _ _ S _ _ _ G _ _ _
-                            _ _ _ _ _ _ _ E S T U A R Y _
-                            _ _ _ _ _ _ _ D _ _ _ R _ _ _
-                            _ _ _ _ _ _ _ _ _ _ _ L _ _ _
-                            _ _ _ _ _ _ _ _ _ _ _ I _ _ _
-                            _ _ _ _ _ _ _ _ _ _ _ C _ _ _
-                            _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " })
+# Game.create({ name: 'Friday 3AM', completed: false,
+#               current_player: 0,
+#               players:  [
+#                          { 'name': 'Dave', 'score': '18', 'current_letters': 'RXDFOEB' },
+#                          { 'name': 'Deike', 'score': '22', 'current_letters': 'LTAUWVQ' },
+#                          { 'name': 'Luna', 'score': '35', 'current_letters': 'ZHGNIOC' },
+#                          { 'name': 'Ed', 'score': '34', 'current_letters': 'USYKTER' }
+#                          ],
+#               letter_grid: "_ _ _ _ _ _ B R A I N _ _ _ _
+#                             _ _ _ _ _ _ O _ _ D _ _ _ _ _
+#                             _ _ _ _ C R A S H _ _ _ _ _ _
+#                             _ _ _ _ O _ _ U _ _ _ _ _ _ _
+#                             _ _ _ _ O _ _ R _ _ _ _ _ _ _
+#                             _ _ G A L L O P _ _ _ _ _ _ _
+#                             _ _ _ _ _ _ _ R _ _ _ _ _ _ _
+#                             _ _ _ _ _ _ _ I _ _ _ _ _ _ _
+#                             _ _ _ _ _ _ _ S _ _ _ G _ _ _
+#                             _ _ _ _ _ _ _ E S T U A R Y _
+#                             _ _ _ _ _ _ _ D _ _ _ R _ _ _
+#                             _ _ _ _ _ _ _ _ _ _ _ L _ _ _
+#                             _ _ _ _ _ _ _ _ _ _ _ I _ _ _
+#                             _ _ _ _ _ _ _ _ _ _ _ C _ _ _
+#                             _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " })
 
-Game.create({ name: 'Kita Invitational', completed: false,
-              current_player: 0,
-              players:  [
-                         { 'name': 'Dave', 'score': '118', 'current_letters': 'RXDFOEB' },
-                         { 'name': 'Ed', 'score': '218', 'current_letters': 'USYKTER' }
-                         ],
-              letter_grid: "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-                            _ _ _ _ I _ P _ _ _ _ _ _ _ _
-                            _ _ _ _ M _ O M N I V O R E _
-                            _ _ _ _ P O O _ _ _ _ _ _ _ _
-                            _ _ _ _ R _ P _ _ _ _ _ _ _ _
-                            _ _ _ _ E _ _ _ _ _ _ _ _ _ _
-                            _ _ _ P S Y C H I C _ _ _ _ _
-                            _ _ _ _ S _ _ _ _ A _ _ _ _ _
-                            _ _ _ _ I _ _ _ _ C _ _ _ _ _
-                            _ _ B O O G E R _ A _ _ _ _ _
-                            _ _ U _ N _ _ _ _ _ _ _ _ _ _
-                            _ _ T _ A _ _ _ _ _ _ _ _ _ _
-                            _ _ T _ B _ _ _ _ _ _ _ _ _ _
-                            _ _ _ _ L _ _ _ _ _ _ _ _ _ _
-                            _ _ _ _ E _ _ _ _ _ _ _ _ _ _ " })
+# Game.create({ name: 'Kita Invitational', completed: false,
+#               current_player: 0,
+#               players:  [
+#                          { 'name': 'Dave', 'score': '118', 'current_letters': 'RXDFOEB' },
+#                          { 'name': 'Ed', 'score': '218', 'current_letters': 'USYKTER' }
+#                          ],
+#               letter_grid: "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ _ I _ P _ _ _ _ _ _ _ _
+#                             _ _ _ _ M _ O M N I V O R E _
+#                             _ _ _ _ P O O _ _ _ _ _ _ _ _
+#                             _ _ _ _ R _ P _ _ _ _ _ _ _ _
+#                             _ _ _ _ E _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ P S Y C H I C _ _ _ _ _
+#                             _ _ _ _ S _ _ _ _ A _ _ _ _ _
+#                             _ _ _ _ I _ _ _ _ C _ _ _ _ _
+#                             _ _ B O O G E R _ A _ _ _ _ _
+#                             _ _ U _ N _ _ _ _ _ _ _ _ _ _
+#                             _ _ T _ A _ _ _ _ _ _ _ _ _ _
+#                             _ _ T _ B _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ _ L _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ _ E _ _ _ _ _ _ _ _ _ _ " })
 
-Game.create({ name: 'Regional Semifinals', completed: false,
-              players: [
-                      { 'name': 'Dumbledore', 'score': '2118', 'current_letters': 'EXYAEGO' },
-                      { 'name': 'Deike', 'score': '2331', 'current_letters': 'WOTOCDK' },
-                      { 'name': 'Voldemort', 'score': '1938', 'current_letters': 'RELCOWT' },
-                      { 'name': 'Ornette', 'score': '2938', 'current_letters': 'DEELFOR' }
-                      ],
-              letter_grid: "F R O G _ _ _ _ _ _ _ _ _ _ _
-                            _ E _ _ _ _ _ _ _ _ _ _ _ _ _
-                            _ M _ _ C R A S H _ _ _ _ _ _
-                            _ A _ _ O _ _ _ _ _ _ _ _ _ _
-                            _ I _ _ V _ _ _ _ _ _ _ _ _ _
-                            _ N E V E R _ _ _ _ _ _ _ _ _
-                            _ _ _ E _ _ _ _ _ _ _ _ _ _ _
-                            _ _ _ N _ _ _ _ _ _ _ _ _ _ _
-                            _ _ _ A _ _ _ _ _ _ _ _ _ _ _
-                            _ _ _ L _ _ _ _ _ _ _ _ _ _ _
-                            _ _ _ I _ _ _ _ _ _ _ _ _ _ _
-                            _ _ _ T _ _ _ _ _ _ _ _ _ _ _
-                            _ _ _ Y _ _ _ _ _ _ _ _ _ _ _
-                            _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-                            _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " })
+# Game.create({ name: 'Regional Semifinals', completed: false,
+#               players: [
+#                       { 'name': 'Dumbledore', 'score': '2118', 'current_letters': 'EXYAEGO' },
+#                       { 'name': 'Deike', 'score': '2331', 'current_letters': 'WOTOCDK' },
+#                       { 'name': 'Voldemort', 'score': '1938', 'current_letters': 'RELCOWT' },
+#                       { 'name': 'Ornette', 'score': '2938', 'current_letters': 'DEELFOR' }
+#                       ],
+#               letter_grid: "F R O G _ _ _ _ _ _ _ _ _ _ _
+#                             _ E _ _ _ _ _ _ _ _ _ _ _ _ _
+#                             _ M _ _ C R A S H _ _ _ _ _ _
+#                             _ A _ _ O _ _ _ _ _ _ _ _ _ _
+#                             _ I _ _ V _ _ _ _ _ _ _ _ _ _
+#                             _ N E V E R _ _ _ _ _ _ _ _ _
+#                             _ _ _ E _ _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ N _ _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ A _ _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ L _ _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ I _ _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ T _ _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ Y _ _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+#                             _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " })
